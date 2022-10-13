f05a22bd3710b683f0a6ac5628f78aa56034fff5
Edgar Knapp <Edgar.Knapp@simcorp.com>
2022-10-13 13:09:43 +0200

    test

--- src/math.ts
+++ src/math.ts
@@ -1,14 +1,18 @@
 // ### LEVEL 1: The Basics ###
 export function add(x: number, y: number): number {
   // TODO: Return the sum of x and y
-  throw new Error("Not implemented");
+  return x  + y;
 }
 
 // ### LEVEL 2: Fizz Buzz ###
 export function fizzBuzz(value: number): string {
   // TODO: Return Fizz if value is divisible by 3, Buzz if value is divisible by 5,
   //       FizzBuzz if value is divisible by 3 and 5, and the value as a string otherwise
-  throw new Error("Not implemented");
+  var response = "";
+  if (value % 3 == 0) response += "Fizz";
+  if (value % 5 == 0) response += "Buzz";
+  if (response == "") response = value.toString();
+  return response;
 }
 
 // ### LEVEL 3: Length of vector ###
